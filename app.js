6ad7864be96827d3da8fd9f705ec9a1fc7482b68
jhalim17 <justin.halim17@gmail.com>
2025-08-14 04:01:46 +0700

    feat(app): increase request body size limits for JSON and URL-encoded data

--- app.js
+++ app.js
@@ -50,7 +50,8 @@ const app = express();
 
 app.use(cors());
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 app.get('/', (req, res) => {
   res.status(200).json({
